3a777c8135be7ab1e132c1d41457698bf2a40e75
anekk <aneckalarionova@gmail.com>
2026-03-19 11:17:43 +0500

    Разработка окна поставщика. Казаков

---
+++ Classes/Manufacturer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylRecordsApplication_2.Classes
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountryCode { get; set; }
+        public string Phone {  get; set; }
+        public string Mail {  get; set; }
+
+        public static IEnumerable<Manufacturer> AllManufacturers()
+        {
+            List<Manufacturer> manufacturers = new List<Manufacturer>();
+            DataTable recordQuery = Classes.DBConnection.Connection("SELECT * FROM [dbo].[Manufacturer]");
+            foreach (DataRow row in recordQuery.Rows)
+                manufacturers.Add(new Manufacturer()
+                {
+                    Id = Convert.ToInt32(row["0"]),
+                    Name = Convert.ToString(row["1"]),
+                    CountryCode = Convert.ToInt32(row["2"]),
+                    Phone = Convert.ToString(row["3"]),
+                    Mail = Convert.ToString(row["4"]),
+                });
+            return manufacturers;
+        }
+
+        public void Save(bool Update = false)
+        {
+            if (Update == false)
+            {
+                Classes.DBConnection.Connection($"INSERT INTO [dbo].[Manufacturer]([Name], [CountryCode], [Phone], [Mail]) VALUES ('{this.Name}', {this.CountryCode}, '{this.Phone}', '{this.Mail}')");
+
+                this.Id = Manufacturer.AllManufacturers().Where(x => x.Name == this.Name && x.CountryCode == this.CountryCode && this.Phone == this.Phone && this.Mail == this.Mail).First().Id;
+            }
+            else
+            {
+                Classes.DBConnection.Connection($"UPDATE [dbo].[Manufacturer] SET [Name] = '{this.Name}', [CountryCode] = {this.CountryCode}, [Phone] = '{this.Phone}', [Mail] = '{this.Mail}' WHERE [Id] = {this.Id}");
+            }
+        }
+
+        public void Delete() => Classes.DBConnection.Connection($"DELETE FROM [dbo].[Manufacturer] WHERE [Id] = {this.Id};");
+    }
+}
